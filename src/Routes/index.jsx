00b1acb0b6b4d1d01745ea22a928393d1ad5c42b
GunClayton <114902151+Smartsoil-Media@users.noreply.github.com>
2023-04-16 21:57:52 +0800

    Styles

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,11 +1,13 @@
-import {
-    createBrowserRouter
-  } from "react-router-dom";
-  import Login from "../pages/login";
+import { createBrowserRouter } from "react-router-dom";
+import Login from "../pages/Login";
 
 export const router = createBrowserRouter([
-    {
-      path: "/login",
-      element: <Login/>,
-    },
-  ]);
+  {
+    path: "",
+    element: <Login />,
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+  },
+]);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import LoginComponent from '../components/LoginComponent'
 
 export default function Login() {
